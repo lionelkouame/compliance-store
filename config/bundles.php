1545690c6eb9b7e2eb16815a1a3eacb79f8b55aa
Lionel KOUAME <lionelkouame@gmail.com>
2026-07-25 01:55:52 +0200

    feat: integrate TwigBundle, enable API Platform Swagger UI, and add base template support

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
